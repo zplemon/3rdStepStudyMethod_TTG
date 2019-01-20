dc5069778ad429b7f33e9397bef1eeb7da6d2271
zhangpeng6 <zhangpeng6@xiaomi.com>
2019-01-20 22:20:18 +0800

    update git test
    Signed-off-by: zhangpeng6 <zhangpeng6@xiaomi.com>

---
+++ src/main/java/com/demo/entity/User.java
@@ -0,0 +1,20 @@
+package com.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
